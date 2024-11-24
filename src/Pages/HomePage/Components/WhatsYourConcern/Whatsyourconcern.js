4b846700485dd2a18ef3c0ab844becb610a49bcf
pavankumar0826 <pavankumaruiux@gmail.com>
2024-11-24 14:07:54 +0530

    change

--- src/Pages/HomePage/Components/WhatsYourConcern/Whatsyourconcern.js
+++ src/Pages/HomePage/Components/WhatsYourConcern/Whatsyourconcern.js
@@ -35,6 +35,30 @@ function Whatsyourconcern() {
                         <img src={require('../../../../assets/Images/HomePageImages/whatsyourconcern6.png')} />
                         <p>Sexual Wellness</p>
                     </div>
+                    <div className="image">
+                        <img src={require('../../../../assets/Images/HomePageImages/whatsyourconcern1.png')} />
+                        <p>Uncomfortable Periods</p>
+                    </div>
+                    <div className="image">
+                        <img src={require('../../../../assets/Images/HomePageImages/whatsyourconcern2.png')} />
+                        <p>Hair Removal</p>
+                    </div>
+                    <div className="image">
+                        <img src={require('../../../../assets/Images/HomePageImages/whatsyourconcern3.png')} />
+                        <p>Unhygienic Toilet</p>
+                    </div>
+                    <div className="image">
+                        <img src={require('../../../../assets/Images/HomePageImages/whatsyourconcern4.png')} />
+                        <p>Intimate Area irritation</p>
+                    </div>
+                    <div className="image">
+                        <img src={require('../../../../assets/Images/HomePageImages/whatsyourconcern5.png')} />
+                        <p>Personal Safety</p>
+                    </div>
+                    <div className="image">
+                        <img src={require('../../../../assets/Images/HomePageImages/whatsyourconcern6.png')} />
+                        <p>Sexual Wellness</p>
+                    </div>
                 </div>
             </div>
         </>
